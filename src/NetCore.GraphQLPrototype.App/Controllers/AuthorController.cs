d02f5b39c0c256fba625b636d3fcfa4e600f4b63
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-09-11 17:15:17 +0300

    Add endpoints for Author, Book, and Publisher entities

---
+++ src/NetCore.GraphQLPrototype.App/Controllers/AuthorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.GraphQLPrototype.Data.Entities;
+using NetCore.GraphQLPrototype.Data.Services.Interfaces;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NetCore.GraphQLPrototype.App.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public sealed class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService authorService;
+        private readonly IBookService bookService;
+
+        public AuthorController(IAuthorService authorService, IBookService bookService)
+        {
+            this.authorService = authorService;
+            this.bookService = bookService;
+        }
+
+        [HttpGet]
+        [Route("api/v1/authors")]
+        [ProducesResponseType(typeof(IEnumerable<Author>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAuthorsAsync()
+        {
+            var authors = await authorService.GetAuthorsAsync();
+
+            return Ok(authors);
+        }
+
+        [HttpGet]
+        [Route("api/v1/authors/{authorId:int}")]
+        [ProducesResponseType(typeof(Author), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAuthorByIdAsync(int authorId)
+        {
+            var author = await authorService.GetAuthorByIdAsync(authorId);
+
+            return Ok(author);
+        }
+
+        [HttpGet]
+        [Route("api/v1/authors/{authorId:int}/books")]
+        [ProducesResponseType(typeof(IEnumerable<Book>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBooksByAuthorIdAsync(int authorId)
+        {
+            var books = await bookService.GetBooksByAuthorIdAsync(authorId);
+
+            return Ok(books);
+        }
+    }
+}
---
+++ src/NetCore.GraphQLPrototype.App/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.GraphQLPrototype.Data.Entities;
+using NetCore.GraphQLPrototype.Data.Services.Interfaces;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NetCore.GraphQLPrototype.App.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public sealed class BookController : ControllerBase
+    {
+        private readonly IBookService bookService;
+
+        public BookController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+        [HttpGet]
+        [Route("api/v1/books")]
+        [ProducesResponseType(typeof(IEnumerable<Book>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBooksAsync()
+        {
+            var books = await bookService.GetBooksAsync();
+
+            return Ok(books);
+        }
+
+        [HttpGet]
+        [Route("api/v1/books/{isbn}")]
+        [ProducesResponseType(typeof(Book), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBookByIsbnAsync(string isbn)
+        {
+            var book = await bookService.GetBookByIsbnAsync(isbn);
+
+            return Ok(book);
+        }
+    }
+}
